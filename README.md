ad5f2f21e80d5f658d9ff5697fab0ddc7d2a44a9
samir <samirdangare191@gmail.com>
2022-10-04 19:02:10 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# YearEndCounter-Website
---
+++ main.js
@@ -0,0 +1,21 @@
+const body = document.body;
+const endTime = new Date('December 31 2020 23:59:59');
+const daysEl = document.getElementById('days');
+const hoursEl = document.getElementById('hours');
+const minutesEl = document.getElementById('minutes');
+const secondsEl = document.getElementById('seconds');
+
+const updateCountdown = () => {
+	const startTime = new Date();
+	const diff = endTime - startTime;
+	const days = Math.floor(diff / 1000 / 60 / 60 / 24);
+	const hours = Math.floor(diff / 1000 / 60 / 60) % 24;
+	const minutes = Math.floor(diff / 1000 / 60) % 60;
+	const seconds = Math.floor(diff / 1000) % 60;
+	daysEl.innerHTML = days;
+	hoursEl.innerHTML =hours;
+	minutesEl.innerHTML = minutes;
+	secondsEl.innerHTML = seconds;
+}
+
+setInterval(updateCountdown, 1000);
